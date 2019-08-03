f55e9ac6761305c639642cb5781d00fb70e575c8
sunjincheng121 <sunjincheng121@gmail.com>
2019-08-03 16:14:09 +0200

    [FLINK-13471][table] Support TableAggregate in some MetadataHandle.

--- flink-table/flink-table-planner-blink/src/test/scala/org/apache/flink/table/planner/plan/metadata/FlinkRelMdModifiedMonotonicityTest.scala
+++ flink-table/flink-table-planner-blink/src/test/scala/org/apache/flink/table/planner/plan/metadata/FlinkRelMdModifiedMonotonicityTest.scala
@@ -20,7 +20,7 @@ package org.apache.flink.table.planner.plan.metadata
 
 import org.apache.flink.table.planner.functions.sql.FlinkSqlOperatorTable
 import org.apache.flink.table.planner.plan.`trait`.RelModifiedMonotonicity
-import org.apache.flink.table.planner.plan.nodes.logical.FlinkLogicalRank
+import org.apache.flink.table.planner.plan.nodes.logical.{FlinkLogicalRank, FlinkLogicalTableAggregate}
 import org.apache.flink.table.runtime.operators.rank.{ConstantRankRange, RankType}
 
 import org.apache.calcite.rel.RelCollations
@@ -31,6 +31,8 @@ import org.apache.calcite.sql.validate.SqlMonotonicity._
 import org.apache.calcite.util.ImmutableBitSet
 import org.junit.Assert._
 import org.junit.Test
+
+import scala.collection.JavaConversions._
 
 class FlinkRelMdModifiedMonotonicityTest extends FlinkRelMdHandlerTestBase {
 
@@ -117,6 +119,34 @@ class FlinkRelMdModifiedMonotonicityTest extends FlinkRelMdHandlerTestBase {
       outputRankNumber = false
     )
     assertEquals(null, mq.getRelModifiedMonotonicity(rank))
+  }
+
+  @Test
+  def testGetRelMonotonicityOnTableAggregateAfterScan(): Unit = {
+    assertEquals(
+      new RelModifiedMonotonicity(Array(CONSTANT, NOT_MONOTONIC, NOT_MONOTONIC)),
+      mq.getRelModifiedMonotonicity(logicalTableAgg))
+  }
+
+  @Test
+  def testGetRelMonotonicityOnTableAggregateAfterAggregate(): Unit = {
+    val projectWithMaxAgg = relBuilder.scan("MyTable4")
+      .aggregate(
+        relBuilder.groupKey(relBuilder.field("a"), relBuilder.field("b")),
+        relBuilder.max("max_c", relBuilder.field("c")),
+        relBuilder.sum(false, "sum_d", relBuilder.field("d")))
+      .project(relBuilder.field(2), relBuilder.field(1))
+      .build()
+
+    val tableAggregate = new FlinkLogicalTableAggregate(
+      cluster,
+      logicalTraits,
+      projectWithMaxAgg,
+      ImmutableBitSet.of(0),
+      null,
+      Seq(tableAggCall)
+    )
+    assertEquals(null, mq.getRelModifiedMonotonicity(tableAggregate))
   }
 
   @Test
